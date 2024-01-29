59459b13e06e32e09e2aca2d6afcbd3d10bc55c2
Crowdbotics <team@crowdbotics.com>
2024-01-29 04:59:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delta-skycoompanion-45653",
+  url: "https://delta-skycoompanion-45653.botics.co",
+  api: "https://delta-skycoompanion-45653.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
